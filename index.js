700b871dc1b2e71e236e76f30db4139d14f64ae8
Samson Njai <njaisamson@gmail.com>
2024-06-25 03:03:38 +0300

    js maths operations

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+
+const multiply = num1 * num2;
+
+const random = Math.round(Math.random() * 10);
+
+let num3 = 14;
+let num4 = 5;
+const mod = num3 % num4;
+
+const max = Math.max(13, 17, 20, 5, 10);
